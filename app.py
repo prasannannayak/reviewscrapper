9d32f8093439166fff0939cebd46bc305bd092f3
prasannannayak <nayakprasannak@gmail.com>
2022-04-10 12:27:36 +0530

    this is my first commit

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def index():
             print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
+
             filename = searchString + ".csv"
             fw = open(filename, "w")
             headers = "Product, Customer Name, Rating, Heading, Comment \n"
@@ -79,5 +80,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
+    #app.run(host='127.0.0.1', port=9000, debug=True)
 	app.run(debug=True)
